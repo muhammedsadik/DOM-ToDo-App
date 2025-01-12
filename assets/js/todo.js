7f77f499484d6faac8b796a9f971ba85eb7cf083
Sadık Orak <muhammedsadikorak@gmail.com>
2025-01-12 22:12:02 +0300

    project finished.

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -2,19 +2,19 @@ const emptyMsg = "Task cannot be empty";
 
 let toDoList = [];
 
-
+taskFormHtml();
 const content = document.querySelector(".content");
 const taskInfo = document.querySelector("#info-text");
 
-
 const newTask = document.querySelector(".task-form");
 newTask.addEventListener("submit", handleNewTask);
+
 
 function handleNewTask(e) {
   e.preventDefault();
   const newTaskData = new FormData(newTask);
   const taskObj = Object.fromEntries(newTaskData);
-  if (taskObj.task=="") {
+  if (taskObj.task.trim() == "") {
     alert(emptyMsg);
     return;
   }
@@ -22,7 +22,7 @@ function handleNewTask(e) {
   if (toDoList.length === 0) {
     taskObj.id = 1;
   } else {
-    taskObj.id = toDoList[0].id +1
+    taskObj.id = toDoList[0].id + 1
   }
   toDoList.unshift(taskObj);
 
@@ -33,22 +33,63 @@ function handleNewTask(e) {
 
 function deleteTask(taskId) {
   const taskIndex = toDoList.findIndex(t => t.id == taskId);
-  toDoList.splice(taskIndex,1);
+  toDoList.splice(taskIndex, 1);
 
   render();
 }
 
+function editTask(e) {
+
+  e.preventDefault();
+
+  const editTaskData = new FormData(e.target);
+  const taskObj = Object.fromEntries(editTaskData);
+  if (taskObj.task.trim() == "") {
+    alert(emptyMsg);
+    return;
+  }
+
+  const editTask = toDoList.find(t => t.id == taskObj.id);
+  editTask.task = taskObj.task;
+
+  taskFormHtml();
+  
+  render();
+}
+
 const handleDeleteTask = (e) => {
-  e.preventDefault();  
+  e.preventDefault();
   deleteTask(e.currentTarget.dataset.id);
+}
+
+function handleEditTask(e) {
+  e.preventDefault();
+
+  const taskId = e.currentTarget.dataset.id;
+  const task = toDoList.find(t => t.id == taskId);
+  const editTaskForm = editTaskHtml(taskId, task.task);
+
+  const input = document.querySelector(".input");
+  input.innerHTML = editTaskForm;
+
+  bindElements();
 }
 
 function bindElements() {
 
   const deleteTaskBtns = document.querySelectorAll("#content-item-delete");
-
   for (const deleteTaskBtn of deleteTaskBtns) {
     deleteTaskBtn.addEventListener("click", handleDeleteTask);
+  }
+
+  const editTasks = document.querySelectorAll(".task-text");
+  for (const text of editTasks) {
+    text.addEventListener("click", handleEditTask);
+  }
+
+  const editNewTask = document.querySelector(".edit-task-form");
+  if (editNewTask) {
+    editNewTask.addEventListener("submit", editTask);
   }
 
 }
@@ -63,12 +104,44 @@ function render() {
 
   numberOfTask();
   bindElements();
-
 }
 
 function numberOfTask() {
   const numberTask = toDoList.length;
   taskInfo.innerHTML = `${numberTask} items left`;
+}
+
+function taskFormHtml(){
+  const input = document.querySelector(".input");
+  input.innerHTML =
+  `
+    <form class="task-form">
+      <button id="input-button" type="submit"></button>
+      <input name="task" id="input-text" type="text" placeholder="Create a new todo…">
+    </form>
+  `;
+}
+
+// function taskHtml(){
+
+//   const input = document.querySelector(".input");
+//   input.innerHTML = taskHtml();
+//   return `
+//       <form class="task-form">
+//         <button id="input-button" type="submit"></button>
+//         <input name="task" id="input-text" type="text" placeholder="Create a new todo…">
+//       </form>
+//   `;
+// }
+
+function editTaskHtml(id, task) {
+  return `
+    <form class="edit-task-form">
+      <input type="hidden" name="id" value="${id}">
+      <button class="edit-input-button" type="submit"></button>
+      <input name="task" class="edit-text" type="text" placeholder="Create a new todo…" value="${task}">
+    </form>
+  `;
 }
 
 function createTaskHtml(id, task) {
@@ -77,7 +150,7 @@ function createTaskHtml(id, task) {
       
         <input type="hidden" name="id" value="${id}">
         <button data-id="${id}" id="content-item-todo"></button>
-        <a href="#">${task}</a>
+        <a data-id="${id}" class="task-text" href="#">${task}</a>
         <button data-id="${id}" id="content-item-delete" type="submit">
           <img src="/assets/img/delete.svg" alt="delete">
         </button>
@@ -85,5 +158,7 @@ function createTaskHtml(id, task) {
       </div>
   `;
 }
+
+
 
 render();
